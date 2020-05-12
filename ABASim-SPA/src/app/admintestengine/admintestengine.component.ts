89c70b9cb870b2a87987212fa58d62e12da22fc2
atempleman <ashley.templeman42@gmail.com>
2020-05-12 14:41:32 +1000

    add box score totals to test run

--- ABASim-SPA/src/app/admintestengine/admintestengine.component.ts
+++ ABASim-SPA/src/app/admintestengine/admintestengine.component.ts
@@ -30,6 +30,38 @@ export class AdmintestengineComponent implements OnInit {
   homeBoxScoreDisplay = 0;
   awayBoxScoreDisplay = 0;
 
+  // Totals
+  awayPoints = 0;
+  awayRebounds = 0;
+  awayAssists = 0;
+  awaySteals = 0;
+  awayBlocks = 0;
+  awayMinutes = 0;
+  awayBlockedAttempts = 0;
+  awayTurnovers = 0;
+  awayFouls = 0;
+  awayFGA = 0;
+  awayFGM = 0;
+  awayFTM = 0;
+  awayFTA = 0;
+  away3FGA = 0;
+  away3FGM = 0;
+  homePoints = 0;
+  homeRebounds = 0;
+  homeAssists = 0;
+  homeSteals = 0;
+  homeBlocks = 0;
+  homeMinutes = 0;
+  homeBlockedAttempts = 0;
+  homeTurnovers = 0;
+  homeFouls = 0;
+  homeFGA = 0;
+  homeFGM = 0;
+  homeFTM = 0;
+  homeFTA = 0;
+  home3FGA = 0;
+  home3FGM = 0;
+
   constructor(private fb: FormBuilder, private engineService: GameEngineService, private alertify: AlertifyService) { }
 
   ngOnInit() {
@@ -84,6 +116,46 @@ export class AdmintestengineComponent implements OnInit {
       this.boxScores = this.boxScores.sort((a, b) => a.minutes < b.minutes ? 1 : a.minutes > b.minutes ? -1 : 0);
       this.homeBoxScores = this.boxScores.filter(bs => bs.teamId === this.homeId);
       this.awayBoxScores = this.boxScores.filter(bs => bs.teamId === this.awayId);
+
+      this.calculateTeamTotals();
+    });
+  }
+
+  calculateTeamTotals() {
+    this.awayBoxScores.forEach(bs => {
+      this.awayFGA = this.awayFGA + bs.fga;
+      this.awayFGM = this.awayFGM + bs.fgm;
+      this.awayFTM = this.awayFTM + bs.ftm;
+      this.awayFTA = this.awayFTA + bs.fta;
+      this.awayPoints = this.awayPoints + bs.points;
+      this.awayRebounds = this.awayRebounds + bs.rebounds;
+      this.awayAssists = this.awayAssists + bs.assists;
+      this.awaySteals = this.awaySteals + bs.steals;
+      this.awayBlocks = this.awayBlocks + bs.blocks;
+      this.awayBlockedAttempts = this.awayBlockedAttempts + bs.blockedAttempts;
+      this.awayMinutes = this.awayMinutes + bs.minutes;
+      this.awayTurnovers = this.awayTurnovers + bs.turnovers;
+      this.awayFouls = this.awayFouls + bs.fouls;
+      this.away3FGA = this.away3FGA + bs.threeFGA;
+      this.away3FGM = this.away3FGM + bs.threeFGM;
+    });
+
+    this.homeBoxScores.forEach(bs => {
+      this.homeFGA = this.homeFGA + bs.fga;
+      this.homeFGM = this.homeFGM + bs.fgm;
+      this.homeFTM = this.homeFTM + bs.ftm;
+      this.homeFTA = this.homeFTA + bs.fta;
+      this.homePoints = this.homePoints + bs.points;
+      this.homeRebounds = this.homeRebounds + bs.rebounds;
+      this.homeAssists = this.homeAssists + bs.assists;
+      this.homeSteals = this.homeSteals + bs.steals;
+      this.homeBlocks = this.homeBlocks + bs.blocks;
+      this.homeBlockedAttempts = this.homeBlockedAttempts + bs.blockedAttempts;
+      this.homeMinutes = this.homeMinutes + bs.minutes;
+      this.homeTurnovers = this.homeTurnovers + bs.turnovers;
+      this.homeFouls = this.homeFouls + bs.fouls;
+      this.home3FGA = this.home3FGA + bs.threeFGA;
+      this.home3FGM = this.home3FGM + bs.threeFGM;
     });
   }
 
