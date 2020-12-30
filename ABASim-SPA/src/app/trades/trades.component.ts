7f16396d7597d06d7b1c5ed4a9e343fc6560a801
atempleman <ashley.templeman42@gmail.com>
2020-12-30 16:06:56 +1000

    team colouring

--- ABASim-SPA/src/app/trades/trades.component.ts
+++ ABASim-SPA/src/app/trades/trades.component.ts
@@ -76,6 +76,9 @@ export class TradesComponent implements OnInit {
   yourTeamRoster: TradePlayerView[] = [];
   selectedTeamRoster: TradePlayerView[] = [];
 
+  primaryColor: string = '22, 24, 100';
+  secondaryColor: string = '12,126,120';
+
   constructor(private alertify: AlertifyService, private router: Router, private teamService: TeamService,
     private transferService: TransferService, private modalService: BsModalService,
     private fb: FormBuilder, private spinner: NgxSpinnerService) { }
@@ -93,6 +96,8 @@ export class TradesComponent implements OnInit {
       this.team = result;
     }, error => {
       this.alertify.error('Error getting your team');
+    }, () => {
+      this.backgroundStyle();
     });
 
     this.teamService.getTradePlayerView(teamId).subscribe(result => {
@@ -664,6 +669,165 @@ export class TradesComponent implements OnInit {
       });
     } else {
       this.modalRef = this.modalService.show(template);
+    }
+  }
+
+  backgroundStyle() {
+    switch (this.team.id) {
+      case 2:
+        // Toronto
+        this.primaryColor = '206,17,65';
+        this.secondaryColor = '6,25,34';
+        break;
+      case 3:
+        // Milwaukee
+        this.primaryColor = '0,71,27';
+        this.secondaryColor = '240,235,210';
+        break;
+      case 4:
+        // Miami
+        this.primaryColor = '152,0,46';
+        this.secondaryColor = '249,160,27';
+        break;
+      case 5:
+        // Denver
+        this.primaryColor = '13,34,64';
+        this.secondaryColor = '255,198,39';
+        break;
+      case 6:
+        // Lakers
+        this.primaryColor = '85,37,130';
+        this.secondaryColor = '253,185,39';
+        break;
+      case 7:
+        // Rockets
+        this.primaryColor = '206,17,65';
+        this.secondaryColor = '6,25,34';
+        break;
+      case 8:
+        // Boston
+        this.primaryColor = '0, 122, 51';
+        this.secondaryColor = '139,111,78';
+        break;
+      case 9:
+        // Indiana
+        this.primaryColor = '0,45,98';
+        this.secondaryColor = '253,187,48';
+        break;
+      case 10:
+        // Orlando
+        this.primaryColor = '0,125,197';
+        this.secondaryColor = '196,206,211';
+        break;
+      case 11:
+        // OKC
+        this.primaryColor = '0,125,195';
+        this.secondaryColor = '239,59,36';
+        break;
+      case 12:
+        // Clippers
+        this.primaryColor = '200,16,46';
+        this.secondaryColor = '29,66,148';
+        break;
+      case 13:
+        // Dallas
+        this.primaryColor = '0,83,188';
+        this.secondaryColor = '0,43,92';
+        break;
+      case 14:
+        // 76ers
+        this.primaryColor = '0,107,182';
+        this.secondaryColor = '237,23,76';
+        break;
+      case 15:
+        // Chicago
+        this.primaryColor = '206,17,65';
+        this.secondaryColor = '6,25,34';
+        break;
+      case 16:
+        // Charlotte
+        this.primaryColor = '29,17,96';
+        this.secondaryColor = '0,120,140';
+        break;
+      case 17:
+        // Utah
+        this.primaryColor = '0,43,92';
+        this.secondaryColor = '0,71,27';
+        break;
+      case 18:
+        // Phoenix
+        this.primaryColor = '29,17,96';
+        this.secondaryColor = '229,95,32';
+        break;
+      case 19:
+        // Memphis
+        this.primaryColor = '93,118,169';
+        this.secondaryColor = '18,23,63';
+        break;
+      case 20:
+        // Brooklyn
+        this.primaryColor = '0,0,0';
+        this.secondaryColor = '119,125,132';
+        break;
+      case 21:
+        // Detroit
+        this.primaryColor = '200,16,46';
+        this.secondaryColor = '29,66,138';
+        break;
+      case 22:
+        // Washington
+        this.primaryColor = '0,43,92';
+        this.secondaryColor = '227,24,55';
+        break;
+      case 23:
+        // Portland
+        this.primaryColor = '224,58,62';
+        this.secondaryColor = '6,25,34';
+        break;
+      case 24:
+        // Sacromento
+        this.primaryColor = '91,43,130';
+        this.secondaryColor = '99,113,122';
+        break;
+      case 25:
+        // Spurs
+        this.primaryColor = '196,206,211';
+        this.secondaryColor = '6,25,34';
+        break;
+      case 26:
+        // Knicks
+        this.primaryColor = '0,107,182';
+        this.secondaryColor = '245,132,38';
+        break;
+      case 27:
+        // Cavs
+        this.primaryColor = '134,0,56';
+        this.secondaryColor = '4,30,66';
+        break;
+      case 28:
+        // Atlanta
+        this.primaryColor = '225,68,52';
+        this.secondaryColor = '196,214,0';
+        break;
+      case 29:
+        // Minnesota
+        this.primaryColor = '12,35,64';
+        this.secondaryColor = '35,97,146';
+        break;
+      case 30:
+        // GSW
+        this.primaryColor = '29,66,138';
+        this.secondaryColor = '255,199,44';
+        break;
+      case 32:
+        // New Orleans
+        this.primaryColor = '0,22,65';
+        this.secondaryColor = '225,58,62';
+        break;
+      default:
+        this.primaryColor = '22, 24, 100';
+        this.secondaryColor = '12, 126, 120';
+        break;
     }
   }
 }
